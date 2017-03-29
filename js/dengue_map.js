81b462a5b68f94e40b11454d4c904ba12855d47a
Rodrigo Gama <rodrigogama@simepar.br>
2017-03-29 12:08:35 -0300

    Gooey animation done.

--- js/dengue_map.js
+++ js/dengue_map.js
@@ -12,17 +12,11 @@
         .attr("viewBox", "0 0 " + width + " " + height)
         .style("max-width", "100%");
 
+    // SVG border
     svg.append("rect")
         .attr({ width: width, height: height })
         .style("fill", "none")
         .style("stroke", "black");
-
-    var projection = d3.geo.mercator()
-        .scale(1100)
-        .translate([1650, 105]);
-
-    var path = d3.geo.path()
-        .projection(projection);
 
     // SVG filter for the gooey effect
     // Code taken from http://tympanus.net/codrops/2015/03/10/creative-gooey-effects/
@@ -46,112 +40,154 @@
         .attr("in2", "gooey")
         .attr("operator", "atop");
 
-    // Scale from 0 to max number of cases of all the time
-    var maxCases = [];
-    dengue_cases.forEach(function(d) {
-        var highest = Number.NEGATIVE_INFINITY;
-        var tmp;
-        // finds the max value of the current state
-        for (var i = d.record.length-1; i >= 0; i--) {
-            tmp = d.record[i].numberOfCases;
-            if (tmp > highest) highest = tmp;
-        }
-        maxCases.push(highest);
-    });
+    // Setting up the map
+    var projection = d3.geo.mercator()
+        .scale(1100)
+        .translate([1650, 105]);
+
+    var path = d3.geo.path()
+        .projection(projection);
+
+    // Creating Brazil's map
+    var map = createMap(svg, path);
 
     // Radius scale
+    var max = getMax(dengue_cases, "numberOfCases");
     var rScale = d3.scale.sqrt()
-        .domain([0, d3.max(maxCases)])
-        .range([0, 20]);
+        .domain([0, max]) // Input: from 0 to max number of cases of all the time
+        .range([0, 20]);  // Output: radius from 0 to 20
 
-    createMap().then(function() {
-        console.log("Done.");
+    // Put the cases locations as well as its radius into the data itself
+    dengue_cases.forEach(function(d) {
+        d.record.forEach(function(e) {
+            e.radius = rScale(e.numberOfCases) * 2;
+        });
+        d.x = projection([d.lat, d.lon])[0];
+        d.y = projection([d.lat, d.lon])[1];
     });
 
-    /////////////////////////////////////
+    // Wrapper for the dengue cases
+    var casesWrapper = svg.append("g")
+        .attr("class", "casesWrapper")
+        .style("filter", "url(#gooeyCodeFilter)");
 
-    function createMap() {
-        var deferred = $.Deferred();
+    // Most recent year, used to show its data first
+    var lastYear = dengue_cases[0].record[dengue_cases[0].record.length-1].year;
 
-        d3.json("data/brazil_states.topo.json", function (error, br) {
-            if (error) throw error;
+    // Get center coordinates of the map, so that all the circles are located at the same place.
+    var bbox = map.node().getBBox();
+    var coordinates = [bbox.x + bbox.width/2, bbox.y + bbox.height/2]; // The center of map
+    
+    // Place the cases circles
+    placeCases(lastYear, coordinates);
+    
+    // Circle over all others
+    var coverCircleRadius = 40;
+    
+    casesWrapper.append("circle")
+        .attr("class", "casesCover")
+        .attr("r", coverCircleRadius)
+        .attr("cx", coordinates[0])
+        .attr("cy", coordinates[1]);
 
-            var states = topojson.feature(br, br.objects.states).features;
+    // Calculate the centers for each region
+    var centers = getCenters("region", [width, height/0.8]);
+    centers.forEach(function(d) {
+        d.y = d.y - 100;
+        d.x = d.x + 0;
+    });
 
-            // Group for holding the map paths
-            svg.map = svg.append("g").attr("class", "map");
-                
-            svg.map.selectAll("path") // Draws all the states
-                .data(states)
-                .enter().append("path")
-                    .attr("d", path)
-                    .attr("class", function(d) { return "state " + d.properties.postal; })
-                    .on("mouseover", function(d) { showTooltip(d.properties.postal); })
-                    .on("mouseout",  function(d) { removeTooltip(d.properties.postal); });
+    // Wrapper for the region labels
+    var labelWrapper = svg.append("g")
+        .attr("class", "labelWrapper");
+        
+    // Append the country labels
+    labelWrapper.selectAll(".label")
+        .data(centers)
+        .enter().append("text")
+        .attr("class", "label")
+        .style("opacity", 0)
+        .attr("transform", function (d) { return "translate(" + (d.x) + ", " + (d.y - 60) + ")"; })
+        .text(function (d) { return d.region });
 
-            svg.map.append("path") // Line between the states
-                .datum(topojson.mesh(br, br.objects.states, function(a, b) { return a !== b; }))
-                .attr("class", "mesh")
-                .attr("d", path);
+    // Set-up the force
+    var force = d3.layout.force()
+        .gravity(.02)
+        .charge(0)
+        .on("tick", tick(centers, "region"));
 
-            // Appends the circle cases on each state
-            showDengueCases();
-        });
-
-        deferred.resolve();
-        return deferred.promise();
+    loop();	
+    setInterval(loop, 15000);
+    
+    function loop() {
+        showCases();
+        setTimeout(clusterByRegion, 7000);
+        setTimeout(backToCenter, 12000);
     }
 
-    function showDengueCases() {
-        var deferred = $.Deferred();
+    /*******************************************************************************************
+     ************************************** Map functions **************************************
+     *******************************************************************************************/
 
-        dengue_cases.forEach(function(state) {
-            var element = d3.select(".state." + state.uf).node(); // Get the DOM element from a D3 selection.
-            var bbox = element.getBBox(); // Use the native SVG interface to get the bounding box
-            var coordinates = [bbox.x + bbox.width/2, bbox.y + bbox.height/2]; // The center of the bounding box
+    /** @function createMap
+     *  @description Creates the geographic map of Brazil.
+    */
+    function createMap(svg, path) {
+        var states = topojson.feature(brazilMap, brazilMap.objects.states).features;
 
-            // Appends circles that represent the number of dengue cases on each state by year.
-            svg.map.selectAll("circle." + state.uf)
-                .data(state.record)
-                .enter().append("circle")
-                    .attr("id", function(d) { return "circle-" + state.uf + "-" + d.year; })
-                    .attr("r",  function(d) { return rScale(d.numberOfCases) * 2; })
-                    .attr("cx", coordinates[0])
-                    .attr("cy", coordinates[1])
-                    .attr("class", "cases")
-                    .style("opacity", 0.85)
-                    .style("visibility", function(d) { return d.year === 2015 ? "visible" : "hidden"; })
-                    .on("mouseover", function() { showTooltip(state.uf); })
-	                .on("mouseout",function() { removeTooltip(state.uf); });
-        });
+        // Group for holding the map paths
+        var map = svg.append("g").attr("class", "map");
+            
+        map.selectAll("path") // Draws all the states
+            .data(states)
+            .enter().append("path")
+                .attr("d", path)
+                .attr("class", function(d) { return "state " + d.properties.postal; })
+                .on("mouseover", function(d) { showTooltip(d.properties.postal); })
+                .on("mouseout",  function(d) { removeTooltip(d.properties.postal); });
 
-        // Recentering paths that are not in a good position.
-        d3.selectAll("[id*=circle-ES]")
-            .attr("cx", function(d) { return parseFloat(d3.select(this).attr("cx")) - 100; });
-        d3.selectAll("[id*=circle-RN]")
-            .attr("cx", function(d) { return parseFloat(d3.select(this).attr("cx")) - 50; })
-            .attr("cy", function(d) { return parseFloat(d3.select(this).attr("cy")) + 50; });
-        d3.selectAll("[id*=circle-PI]")
-            .attr("cx", function(d) { return parseFloat(d3.select(this).attr("cx")) + 20; });
-        d3.selectAll("[id*=circle-SC]")
-            .attr("cx", function(d) { return parseFloat(d3.select(this).attr("cx")) + 20; });
-        d3.selectAll("[id*=circle-RJ]")
-            .attr("cx", function(d) { return parseFloat(d3.select(this).attr("cx")) + 10; })
-            .attr("cy", function(d) { return parseFloat(d3.select(this).attr("cy")) + 5; });
+        map.append("path") // Line between the states
+            .datum(topojson.mesh(brazilMap, brazilMap.objects.states, function(a, b) { return a !== b; }))
+            .attr("class", "mesh")
+            .attr("d", path);
 
-        deferred.resolve();
-        return deferred.promise();
+        return map;
     }
 
-    /***********************************************
-     ************** Tooltip functions **************
-     ***********************************************/
+    /** @function placeCases
+     *  @description Places the circles that corresponds to dengue cases on each state.
+    */
+    function placeCases(year, coordinates) {
+        casesWrapper.selectAll(".cases")
+            .data(dengue_cases)
+            .enter().append("circle")
+                .attr("id", function(d) { return "circle-" + d.uf; })
+                .attr("class", "cases")
+                .attr("r", function(d) {
+                    var obj = d.record.filter(function(e) { return e.year === year; });
+                    return obj[0].radius;
+                })
+                .attr("cx", coordinates[0])
+                .attr("cy", coordinates[1])
+                .on("mouseover", function(d) { showTooltip(d.uf); })
+                .on("mouseout",function(d) { removeTooltip(d.uf); });;
+    }
 
+    /*******************************************************************************************
+     ************************************ Tooltip functions ************************************
+     *******************************************************************************************/
+    /** @function showTooltip
+     *  @description Shows a tooltip with the number of dengue cases for a given state.
+    */
     function showTooltip(state) {
         var cases = 0;
-        var element = d3.selectAll("circle[id^='circle-"+ state +"']").filter(function(d) {
-            cases = d.numberOfCases;
-            return this.style.visibility === "visible";
+        var element = d3.select("#circle-" + state);
+
+        dengue_cases.forEach(function(d) {
+            d.record.forEach(function(e) {
+                if (e.year === lastYear && d.uf === state)
+                    cases = e.numberOfCases; 
+            });
         });
 
         // Define and show the tooltip using bootstrap popover;
@@ -169,10 +205,11 @@
         element.style("opacity", 1);
     }
 
+    /** @function removeTooltip
+     *  @description Hides a tooltip with the number of dengue cases for a given state.
+    */
     function removeTooltip(state) {
-        var element = d3.selectAll("circle[id^='circle-"+ state +"']").filter(function(d) {
-            return this.style.visibility === "visible";
-        });
+        var element = d3.select("#circle-" + state);
 
         // Hide the tooltip
         $(".popover").each(function() {
@@ -181,6 +218,211 @@
         
         // Fade out the bright circle again
         element.style("opacity", 0.85);   
+    }
+
+    /*******************************************************************************************
+     *********************************** Animation functions ***********************************
+     *******************************************************************************************/
+    /** @function placeCases
+     *  @description Moves the cases of dengue from the center to their actual states within Brazil.
+    */
+    function showCases() {
+        // Stop the force layout (in case you move backward)
+        force.stop();
+
+        // Make the cover circle shrink
+        d3.selectAll(".casesCover")
+            .transition().duration(2000)
+            .attr("r", 0);
+
+        // Put the cases in center of each state
+        d3.selectAll(".cases")
+            .transition("move").duration(2000)
+            .delay(function(d, i) { return i * 20; })
+            .attr("r", function(d) {
+                var obj = d.record.filter(function(e) { return e.year === lastYear; });
+                return obj[0].radius = obj[0].radius;
+            })
+            .attr("cx", function(d) { return d.x = projection([d.lat, d.lon])[0]; })
+            .attr("cy", function(d) { return d.y = projection([d.lat, d.lon])[1]; });
+
+        // Around the end of the transition above make the circles see-through a bit
+        d3.selectAll(".cases")
+            // .transition("dim").duration(2000).delay(4000)
+            // .style("opacity", 0.85)
+            .transition().duration(2000).delay(1000)
+            .style("opacity", 0.85)
+            .style("stroke-opacity", 1);
+            
+
+        // "Remove" gooey filter from cities during the transition
+        // So at the end they do not appear to melt together anymore
+        d3.selectAll(".blurValues")
+            .transition().duration(4000)
+            .attrTween("values", function() { 
+                return d3.interpolateString("1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -5", 
+                                            "1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 6 -5"); 
+            });
+    }
+
+    /** @function clusterByRegion
+     *  @description Cluster all the cases based on the region of the state
+    */
+    function clusterByRegion() {
+        // Start force again
+        force.start();
+
+        // Dim the map
+        d3.selectAll(".state")
+            .transition().duration(1000)
+            .style("fill-opacity", 0);
+            
+        // Show the labels
+        d3.selectAll(".label")
+            .transition().duration(500)
+            .style("opacity", 1);
+
+        d3.selectAll(".cases")
+            .transition().duration(1000)
+            .style("opacity", 1).style("stroke-opacity", 0);
+
+        //Reset gooey filter values back to a visible "gooey" effect
+        d3.selectAll(".blurValues")
+            .transition().duration(2000)
+            .attrTween("values", function() { 
+                return d3.interpolateString("1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 6 -5", 
+                                            "1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 35 -6"); 
+            });
+    }
+
+    /** @function backToCenter
+     *  @description Move the circles back to the center location again
+    */
+    function backToCenter() {
+        // Stop the force layout
+        force.stop();
+
+        // Hide labels
+        d3.selectAll(".label")
+            .transition().duration(500)
+            .style("opacity", 0);
+
+        // Show map
+        d3.selectAll(".state")
+            .transition().duration(1000)
+            .style("fill-opacity", 0.8);
+        
+        // Make the cover circle to its true size again
+        d3.selectAll(".casesCover")
+            .transition().duration(3000).delay(500)
+            .attr("r", coverCircleRadius);
+
+        // Move the cities to the 0,0 coordinate
+        d3.selectAll(".cases")
+            .transition().duration(2000)
+            .delay(function(d, i) { return i * 10; })
+            .attr("cx", coordinates[0])
+            .attr("cy", coordinates[1]);
+            
+        d3.selectAll(".blurValues")
+            .transition().duration(1000).delay(1000)
+            .attrTween("values", function() {
+                return d3.interpolateString("1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 35 -6",
+                                            "1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -5");
+            });
+    }
+
+    /*******************************************************************************************
+     ************************************* Helper functions ************************************
+     *******************************************************************************************/
+
+    function getMax(arr, vname) {
+        var max = [];
+        
+        arr.forEach(function(d) {
+            var highest = Number.NEGATIVE_INFINITY;
+            var tmp;
+            // finds the max value of the current state
+            for (var i = d.record.length-1; i >= 0; i--) {
+                tmp = d.record[i][vname];
+                if (tmp > highest) highest = tmp;
+            }
+            max.push(highest);
+        });
+
+        return d3.max(max);
+    }
+
+    function tick(centers, vname) {
+        var foci = {};
+        for (var i=0; i < centers.length; i++)
+            foci[centers[i].region] = centers[i];
+
+        return function (e) {
+            for (var i = 0; i < dengue_cases.length; i++) {
+                var o = dengue_cases[i];
+                var f = foci[o[vname]];
+                o.y += (f.y - o.y) * e.alpha;
+                o.x += (f.x - o.x) * e.alpha;
+            }
+
+            d3.selectAll(".cases")
+                .each(collide(.5))
+                .attr("cx", function (d) { return d.x; })
+                .attr("cy", function (d) { return d.y; });
+        };
+    }
+
+    function getCenters(vname, size) {
+        var centers = [], 
+            mapping,
+            flags = [];
+
+        for (var i=0; i < dengue_cases.length; i++) {
+            if (flags[dengue_cases[i][vname]]) continue;
+            
+            flags[dengue_cases[i][vname]] = true;
+            centers.push({ region: dengue_cases[i][vname], value: 1});
+        }
+
+        centers.sort(function(a, b){ return d3.ascending(a.name, b.name); });
+
+        mapping = d3.layout.pack()
+            .sort(function(d) { return d[vname]; })
+            .size(size);
+        mapping.nodes({children: centers});
+
+        return centers;
+    }
+
+    function collide(alpha) {
+        var quadtree = d3.geom.quadtree(dengue_cases);
+        var padding = 0;
+        var maxRadius = getMax(dengue_cases, "radius")
+
+        return function (d) {
+            var r = d.radius + maxRadius + padding,
+                nx1 = d.x - r,
+                nx2 = d.x + r,
+                ny1 = d.y - r,
+                ny2 = d.y + r;
+            quadtree.visit(function(quad, x1, y1, x2, y2) {
+                if (quad.point && (quad.point !== d)) {
+                var x = d.x - quad.point.x,
+                    y = d.y - quad.point.y,
+                    l = Math.sqrt(x * x + y * y),
+                    r = d.radius + quad.point.radius + padding;
+                if (l < r) {
+                    l = (l - r) / l * alpha;
+                    d.x -= x *= l;
+                    d.y -= y *= l;
+                    quad.point.x += x;
+                    quad.point.y += y;
+                }
+                }
+                return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+            });
+        };
     }
 
 })();
